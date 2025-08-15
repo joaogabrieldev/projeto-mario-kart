62f027501cd8ca31fbd069685976a02ce58c293d
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-08-15 04:32:56 -0300

    Mario Bros Game Commit #1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,69 @@
-//- Combinar interação no terminal e Javascript para esse projeto
+import readline from "readline-sync";
 
-// console.log(process.argv[0]);
-function valorAleatorio(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// //? Classe Modelo
+// class Personagem {
+//   constructor(id, nome, velocidade, manobrabilidade, poder) {
+//     this.id = id;
+//     this.nome = nome;
+//     this.velocidade = velocidade;
+//     this.manobrabilidade = manobrabilidade;
+//     this.poder = poder;
+//   }
+// }
+
+// //? Objetos (Personagens)
+// const mario = new Personagem(1, "Mario", 4, 3, 3);
+// const bowser = new Personagem(2, "Bowser", 5, 2, 5);
+// const peach = new Personagem(3, "Peach", 3, 4, 2);
+// const luigi = new Personagem(4, "Luigi", 3, 4, 4);
+// const yoshi = new Personagem(5, "Yoshi", 2, 4, 3);
+// const donkeyKong = new Personagem(6, "Donkey Kong", 2, 2, 5);
+
+// const personagens = [mario, bowser, peach, luigi, yoshi, donkeyKong];
+
+// //? Gerar o valor do dado
+// async function rollDice() {
+//   const min = 1;
+//   const max = 6;
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+const anonimo = process.argv.indexOf("-a") !== -1;
+const continuarPrograma = readline
+  .question("Digite [S] para continuar e [N] para sair: ", {
+    limit: /^[sSnN]$/i,
+    limitMessage: "Apenas [S] ou [N], por favor",
+  })
+  .toLowerCase();
+
+if (continuarPrograma == "n") {
+  console.log("Para iniciar o jogo novamente digite [npm run dev]");
+  process.exit();
 }
 
-// console.log(valorAleatorio(1, 6));
+console.log("VAMOS INICIAR A CORRIDA🏁");
 
-//? Lógica para gerar o valor do dado
-const min = 1;
-const max = 5;
+//? Sortear Personagem
+// function sortearID(array) {
+//   if (!array || array.length === 0) {
+//     return null;
+//   }
 
-const resultado = Math.floor(Math.random() * (max - min + 1));
-console.log(resultado);
+//   const indiceSorteado = Math.floor(Math.random() * array.length);
+//   return array[indiceSorteado];
+// }
+
+// let player1 = sortearID(personagens);
+// let player2 = sortearID(personagens);
+
+// while (player1.id == player2.id) {
+//   console.log("Sorteando de Novo, Personagens Iguais!");
+//   player2 = sortearID(personagens);
+// }
+
+// console.log(player1.id, player2.id);
+
+// //? Funcão principal do Jogo (Auto Invoke Function)
+// async function main() {
+//   console.log(``);
+// }
